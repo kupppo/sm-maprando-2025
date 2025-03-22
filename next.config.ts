4210b48e770f8647d4a9c66a72e8c24346abb027
kupppo <kupppo@protonmail.com>
2025-03-22 17:39:39 +0000

    add discord link redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/AxWTznKz26',
+        permanent: false,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig

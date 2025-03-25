c4a4f7748b53acc3ff76730cf0e9447f84ac0804
kupppo <kupppo@protonmail.com>
2025-03-25 15:31:45 +0000

    include setting up matches

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -37,7 +37,7 @@ export default async function AdminPage() {
   )
 
   const pendingMatches = allMatches.filter(
-    (match: any) => match.status === 'PENDING' || match.status === 'SCHEDULED',
+    (match: any) => match.status === 'PENDING' || match.status === 'SCHEDULED' || match.status === 'SETTING_UP',
   )
   const matches = await Promise.all(
     pendingMatches.map(async (match: any) => {
